d8168be652fd6c1b7f7f077e6f3184f42fe6cbb7
louis <louislam617@gmail.com>
2022-06-05 20:48:25 +0100

    Updated

--- Tetris.js
+++ Tetris.js
@@ -1,10 +1,24 @@
-let cnv = document.getElementById("cnv");
-let ctx = cnv.getContext("2d");
-let curTetromino =[[1,0], [0,1], [1,1], [2,1]];
+const cnv = document.getElementById("cnv");
+const ctx = cnv.getContext("2d");
+const curTetromino = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+]
+ctx.scale(20, 20);
 
-// ctx.strokeRect(100, 100, 100, 100);
+ctx.fillStyle = '#000';
+ctx.fillRect(0, 0, cnv.width, cnv.height);
 
-for(let i = 11; i < 265; i+= 23){
-    for(let j = 9; j < 446; j+= 23)
-        ctx.strokeRect(i, j, 23, 23);
+function drawTetromino(curTetromino) {
+    curTetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if(value !== 0) {
+                ctx.fillStyle = 'red';
+                ctx.fillRect(x, y, 1, 1);
+            }
+        });
+    });
 }
+
+drawTetromino(curTetromino);
