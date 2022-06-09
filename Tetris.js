a41fda1dc7c306ec25e3c3c709e78b84bb544d7c
louis <louislam617@gmail.com>
2022-06-09 18:06:43 +0100

    code update

--- Tetris.js
+++ Tetris.js
@@ -1,72 +1,159 @@
-const cnv = document.getElementById("cnv");
-const ctx = cnv.getContext("2d");
-ctx.scale(20, 20);
-const curTetromino = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0],
+const cvs = document.getElementById("tetris");
+const ctx = cvs.getContext("2d");
+
+const ROW = 20;
+const COL = COLUMN = 10;
+const SQ = squareSize = 20;
+const VACANT = "#FFF";
+
+// draw a square
+
+function drawSquare(x, y, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x*SQ, y*SQ, SQ, SQ);
+
+    ctx.strokeStyle = "BLACK";
+    ctx.strokeRect(x*SQ, y*SQ, SQ, SQ);
+}
+
+// create the board
+
+let board = [];
+
+for(r = 0; r < ROW; r++){
+    board[r] =[];
+    for(c = 0; c < COL; c++){
+        board[r][c] = VACANT;
+    }
+}
+
+// draw the board
+function drawBoard(){
+    for(r = 0; r < ROW; r++) {
+        for( c = 0; c < COL; c++) {
+            drawSquare(c, r, board[r][c]);
+        }
+    }
+}
+
+drawBoard();
+
+// the pieces and their colors
+const PIECES = [
+    [Z, "red"],
+    [S, "green"],
+    [T, "yellow"],
+    [O, "blue"],
+    [L, "purple"],
+    [I, "cyan"],
+    [J, "orange"]
 ];
 
-function createMatrix(w, h) {
-    const matrix = [];
-    while(h--) {
-        matrix.push(new Array(w).fill(0));
-    }
-    return matrix;
+// initate a piece
+
+let p = new Piece(PIECES[0][0], PIECES[0][1]);
+
+// The Object Piece
+
+function Piece(tetromino, color){
+    this.tetromino = tetromino;
+    this.color = color;
+
+    this.tetrominoN = 0; // we start from the first pattern
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+
+    // we need to control the pieces
+    this.x = 3;
+    this.y = -2;
 }
 
-function draw() {
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, cnv.width, cnv.height);
-    drawTetromino(player.curTetromino, player.pos);
-}
+// fill function
 
-function drawTetromino(curTetromino, offset) {
-    curTetromino.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if(value !== 0) {
-                ctx.fillStyle = 'red';
-                ctx.fillRect(x + offset.x,
-                             y + offset.y, 
-                             1, 1);
+Piece.prototype.fill = function(){
+    for(r = 0; r < this.activeTetromino.length; r++) {
+        for( c = 0; c < this.activeTetromino.length; c++) {
+            // we draw only occupied square
+            if(this.activeTetromino[r][c]){
+                drawSquare(this.x + c, this.y + r, this.color);
             }
-        });
-    });
-}
-
-let dropCounter = 0;
-let dropInterval = 1000;
-
-let lastTime = 0;
-function update(time = 0) {
-    const deltaTime = time - lastTime;
-    lastTime = time
-
-    dropCounter += deltaTime;
-    if(dropCounter > dropInterval) {
-        player.pos.y++;
-        dropCounter = 0;
+        }
     }
-    draw();
-    requestAnimationFrame(update);
 }
 
-const arena = createMatrix(12, 20);
-console.log(arena);
+// draw a piece to the board
 
-const player = {
-    pos: {x: 5, y: 5},
-    curTetromino: curTetromino,
+Piece.prototype.draw = function(){
+   this.fill(this.color);
 }
 
-document.addEventListener('keydown', event => {
-    if (event.keyCode === 37) {
-        player.pos.x--;
-    } else if (event.keyCode === 39) {
-        player.pos.x++;
-    } else if (event.keyCode === 40) {
-        player.pos.y++;
+// undraw a piece
+
+Piece.prototype.unDraw = function(){
+    this.fill(VACANT);
+}
+
+// move down the piece
+
+Piece.prototype.moveDown = function(){
+    this.unDraw();
+    this.y++;
+    this.draw();
+}
+
+// move Right the piece
+
+Piece.prototype.moveRight = function(){
+    this.unDraw();
+    this.x++;
+    this.draw();
+}
+
+// move Left the piece
+
+Piece.prototype.moveLeft = function(){
+    this.unDraw();
+    this.x--;
+    this.draw();
+}
+
+// rotate the piece
+
+Piece.prototype.rotate = function(){
+    this.unDraw();
+    this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+    this.draw();
+}
+
+// CONTROL the piece
+
+document.addEventListener("keydown", CONTROL);
+
+function CONTROL(event){
+    if(event.keyCode == 37){
+        p.moveLeft();
+    }else if(event.keycode == 38){
+        p.rotate();
+    }else if(event.keycode == 39){
+        p.moveRight();
+    }else if(event.keycode == 40){
+        p.moveDown();
     }
-});
+}
 
-update();
+// drop the piece every 1sec
+
+let dropStart = Date.now();
+function drop(){
+    let now = Date.now();
+    let delta = now - dropStart;
+    if(delta > 1000){
+        p.moveDown();
+        dropStart = Date.now();
+    }
+    requestAnimationFrame(drop);
+}
+
+drop();
+
+console.log(p.unDraw);
